78464efbce0bf90b910fe3573b4edadae97512c6
Max <max.khrp@gmail.com>
2022-08-31 22:55:21 +0300

    Testing PR publishing

--- publish/index.ts
+++ publish/index.ts
@@ -1,1 +1,14 @@
-console.log('test!', process.argv);
+import {publish} from './publish';
+
+(async function() {
+  console.log(arguments);
+  // const ghEvent = process.argv[2];
+  // if (ghEvent) {
+  //   const parsedEvent = JSON.parse(ghEvent);
+  //   if (parsedEvent.type) {
+  //     publish([parsedEvent.source], parsedEvent.tag || null);
+  //   }
+  // } else {
+  //   publish(['naj'], null);
+  // }
+})();
