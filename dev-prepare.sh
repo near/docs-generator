b9c2dcd778eb96f04813a85bf7b8bdf90cbcc8b7
Max <max.khrp@gmail.com>
2022-09-01 12:42:39 +0300

    Action generator WIP

--- dev-prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/maxhr/near-api-js ./sources/near-api-js
-cd sources/near-api-js
-git pull
-yarn install
-yarn build
-cd ../../
-mkdir docs
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
   const docsSource = core.getInput('docs-source');
   const releaseVersion = core.getInput('release-version');
+  const token = core.getInput('github_token');
   console.log(`building ${docsSource}@${releaseVersion}`);
+  console.log(JSON.stringify(process.env, undefined, 2))
   const time = (new Date()).toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
