3f87f344e91211cb7fba9e80cd63484d8434dd6f
Max <max.khrp@gmail.com>
2022-09-01 13:47:45 +0300

    Action generator WIP

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import {GitHub, getOctokitOptions} from '@actions/github/lib/utils'
 
-try {
-  const docsSource = core.getInput('docs-source');
-  const releaseVersion = core.getInput('release-version');
-  const token = core.getInput('github_token');
-  console.log(`building ${docsSource}@${releaseVersion}`);
-  console.log(JSON.stringify(process.env, undefined, 2))
-  const time = (new Date()).toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const glob = require('@actions/glob');
+const http = require('@actions/http-client');
+const io = require('@actions/io');
+const tc = require('@actions/tool-cache');
+const artifact = require('@actions/artifact');
+const cache = require('@actions/cache');
+const github = require('@actions/github');
+const {restEndpointMethods} = require('@octokit/plugin-rest-endpoint-methods');
+const restGithub = GitHub.plugin(restEndpointMethods);
+
+(async () => {
+  try {
+    const docsSource = core.getInput('docs-source');
+    const releaseVersion = core.getInput('release-version');
+    const githubToken = core.getInput('github_token');
+
+    const octokit = new restGithub(githubToken);
+    console.log(`building ${docsSource}@${releaseVersion}`);
+    console.log(JSON.stringify(process.env, undefined, 2))
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+    const {data: pullRequest} = await octokit.rest.pulls.get({
+      owner: 'near',
+      repo: 'docs',
+      pull_number: 100,
+      mediaType: {
+        format: 'diff'
+      }
+    });
+    console.log(pullRequest);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
