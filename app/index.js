a866ff2c6c94abb2e4f7729a54845bd12494f630
Max <max.khrp@gmail.com>
2022-09-02 15:55:08 +0300

    Action generator WIP

---
+++ app/index.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+  app.log("Yay! The app was loaded!");
+
+  app.on("issues.opened", async (context) => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Hello, World!" })
+    );
+  });
+};
